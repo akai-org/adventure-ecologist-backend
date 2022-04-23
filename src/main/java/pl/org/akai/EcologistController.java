d56e4bd3ede355f7928496d96bf8e00b533140f9
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-23 10:27:20 +0200

    create controller

--- src/main/java/pl/org/akai/EcologistController.java
+++ src/main/java/pl/org/akai/EcologistController.java
@@ -5,12 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class ExampleResource {
+@Path("/api")
+class EcologistController {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
+
 }
