514b5134298100abde36a4f5387694ef55f7b9e2
Szczepaniak-M <m.szczepaniak99@gmail.com>
2022-04-23 10:20:08 +0200

    Initialise project

---
+++ src/test/java/pl/org/akai/ExampleResourceIT.java
@@ -0,0 +1,9 @@
+package pl.org.akai;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
