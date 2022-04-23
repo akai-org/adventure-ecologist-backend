b4ad5f3c59f76631bc1f661f9b14faf3bbbfc8bb
Szczepaniak-M <m.szczepaniak99@gmail.com>
2022-04-23 11:25:19 +0200

    Add Liquibase changelog

--- src/main/java/pl/org/akai/database/GameStateEntity.java
+++ src/main/java/pl/org/akai/database/GameStateEntity.java
@@ -4,11 +4,14 @@ import io.quarkus.mongodb.panache.common.MongoEntity;
 import lombok.Data;
 import org.bson.types.ObjectId;
 
+import java.time.Instant;
+
 @Data
 @MongoEntity(collection="ThePerson")
 public class GameStateEntity {
     private ObjectId id;
     private String email;
+    private Instant lastLogin;
     private Integer a;
     private Integer b;
     private Integer c;
