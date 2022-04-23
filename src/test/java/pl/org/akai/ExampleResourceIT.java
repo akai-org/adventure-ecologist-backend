af1a8ad780dbf8aa69ebad0f5aa6e8bae31cf2bb
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-23 10:35:21 +0200

    delete tests

--- src/test/java/pl/org/akai/ExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package pl.org.akai;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
